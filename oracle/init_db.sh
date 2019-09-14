6ff0abe8c9fe98a2e9d567d0c43528388a6181ac
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2019-09-14 15:41:49 +0900

    環境構築

--- oracle/init_db.sh
+++ oracle/init_db.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 
-#CDB作成
-cd ~ && time /etc/init.d/oracledb_ORCLCDB-19c configure
+CMD="$1";shift;
+PDB_CNT="$@"
 
-#PDB作成
-sqlplus sys/ORACLE_PWD@ORCLCDB as sysdba @launch_and_open_db.sql
+create(){
+  [ -z "${PDB_CNT}" ] && echo PDB数を入力してください。 && exit 1
+  time sqlplus sys/ORACLE_PWD@ORCLCDB as sysdba @launch_and_open_db.sql ${PDB_CNT}
+  #リスナー登録
+  echo "ORCLPDB@ = (DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = be807f28f5bd)(PORT = 1521)) (CONNECT_DATA = (SERVER = DEDICATED) (SERVICE_NAME = ORCLPDB@)))" | parallel echo :::: - <(seq ${PDB_CNT}) | awk '{$NF="";RN=sprintf("%02d",NR);print "echo \x22"$0"\x22 | sed -E \x27s;@;"RN";g\x27"}' | bash >> /opt/oracle/product/19c/dbhome_1/network/admin/tnsnames.ora
+  sleep 60
+  #リスナー停止
+  lsnrctl stop
+  sleep 60
+  #リスナー起動
+  lsnrctl start
+  sleep 60
+  #リスナー起動確認
+  lsnrctl status
+}
 
-#リスナー登録
-echo "ORCLPDB@ = (DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = be807f28f5bd)(PORT = 1521)) (CONNECT_DATA = (SERVER = DEDICATED) (SERVICE_NAME = ORCLPDB@)))" | parallel echo :::: - <(seq 2) | awk '{$NF="";RN=sprintf("%02d",NR);print "echo \x22"$0"\x22 | sed -E \x27s;@;"RN";g\x27"}' | bash >> /opt/oracle/product/19c/dbhome_1/network/admin/tnsnames.ora
+configure(){
+  time /etc/init.d/oracledb_ORCLCDB-19c configure
+}
 
-sleep 60
-#リスナー停止
-lsnrctl stop
-sleep 60
-#リスナー起動
-lsnrctl start
-sleep 60
-#リスナー起動確認
-lsnrctl status
+case "${CMD}" in
+    create)
+        create
+    ;;
+    configure)
+        configure
+    ;;
+    *)
+        echo $"Usage: $0 {configure|create}"
+        exit 1
+    ;;
+esac
+
+exit 0
