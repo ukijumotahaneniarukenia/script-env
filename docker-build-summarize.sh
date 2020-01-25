5c166f72bb9875e3c0777969f352ad7d7ff89c39
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-01-26 07:47:43 +0900

    環境構築

--- docker-build-summarize.sh
+++ docker-build-summarize.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+#その日の標準出力ログファイル名を取得
 BUILD_STDOUT_LOG=$(ls -l ~/script_env/docker-build-log | grep -P '^-' | awk '{print $9}' | grep "$(date +%Y-%m-%d)" | grep stdout)
+
+#その日の標準出力ログファイルに各コンテナビルドの詳細ログを追記
 while read tgt;do
   echo $tgt
   cat $tgt | grep -Po 'Step [0-9]{1,}/[0-9]{1,}' | tail -n1 #Step数の抽出
@@ -8,3 +11,7 @@ while read tgt;do
   cat $tgt | grep -E '\s[0-9]{1,}m[0-9]{1,}\.[0-9]{3}s' #経過時間の抽出
   cat $tgt | grep -E 'fail|FAIL|Fail' #異常終了ログの抽出
 done < <(ls -l ~/script_env | grep -P '^d' | awk '{print $9}' | xargs -n1 -I@ echo ~/script_env/@/log) >>~/script_env/docker-build-log/$BUILD_STDOUT_LOG
+
+#コンテナ作成に失敗したdockerイメージを削除
+#後処理
+docker images | awk '$1=="<none>"{print $3}' | xargs -I@ docker rmi
