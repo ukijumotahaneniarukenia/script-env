8d60ae428958f388b7d9db3fda54a16c7bd4fc17
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-01-25 16:39:19 +0900

    環境構築

--- docker-build-crontab.sh
+++ docker-build-crontab.sh
@@ -3,3 +3,7 @@
 cd /home/aine/script_env/apache
 
 time docker build --no-cache -t centos_apache . | tee log
+
+cd /home/aine/script_env/fluentd
+
+time docker build --no-cache -t centos_fluentd . | tee log
