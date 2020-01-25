1b5c35ca330086e81802d1e1250b98fa5c1c4120
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-01-25 23:23:21 +0900

    環境構築

--- docker-build-crontab.sh
+++ docker-build-crontab.sh
@@ -2,7 +2,9 @@
 
 BUILD_START=$(date '+%s')
 
-bash ~/script_env/docker-build-parallel.sh
+bash ~/script_env/docker-build-parallel.sh &
+
+wait
 
 #gitignore整備
 ls -l ~/script_env | grep -P '^d' | awk '{print $9}' | xargs -I@ echo cp ~/script_env/.gitignore ~/script_env/@/.gitignore | sh
