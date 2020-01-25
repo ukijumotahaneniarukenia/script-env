671110a3a243209311aec653902ba1f4a81e5618
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-01-25 22:45:01 +0900

    環境構築

--- docker-build-crontab.sh
+++ docker-build-crontab.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+BUILD_START=$(date '+%s')
+
 bash ~/script_env/docker-build-parallel.sh &
 
 wait $!
@@ -11,3 +13,9 @@ cd ~/script_env
 git add .gitignore
 git add --all *
 git commit -m "環境構築"
+
+BUILD_END=$(date '+%s')
+
+BUILD_ELAPSED=$(expr $BUILD_END - $BUILD_START)
+
+echo $BUILD_ELAPSED
