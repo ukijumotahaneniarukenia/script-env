744e13d530df788e5f5dbdf2851e2fdae5ef723d
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-01-25 22:58:57 +0900

    環境構築

--- docker-build-crontab.sh
+++ docker-build-crontab.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
+git-commit(){
+  #gitignore整備
+  ls -l ~/script_env | grep -P '^d' | awk '{print $9}' | xargs -I@ echo cp ~/script_env/.gitignore ~/script_env/@/.gitignore | sh
+  cd ~/script_env
+  git add .gitignore
+  git add --all *
+  git commit -m "環境構築"
+}
+
 BUILD_START=$(date '+%s')
 
-bash ~/script_env/docker-build-parallel.sh &
-
-#以前に起動したすべてのバッググラウンドジョブの待ち合わせ
-wait
-
-#gitignore整備
-ls -l ~/script_env | grep -P '^d' | awk '{print $9}' | xargs -I@ echo cp ~/script_env/.gitignore ~/script_env/@/.gitignore | sh
-
-cd ~/script_env
-git add .gitignore
-git add --all *
-git commit -m "環境構築"
+bash ~/script_env/docker-build-parallel.sh && git-commit
 
 BUILD_END=$(date '+%s')
 
