3cee6ca2e6cb9106975dac02466a646f7997007a
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-01-26 00:21:37 +0900

    環境構築

--- docker-build-crontab.sh
+++ docker-build-crontab.sh
@@ -4,7 +4,12 @@ BUILD_START=$(date '+%s')
 
 bash ~/script_env/docker-build-parallel.sh &
 
-wait
+printf "waiting for docker build proccess.\n"
+while $(ps aux | grep 'docker build' | grep -vq 'grep')
+do
+  printf "💩"
+  sleep 1
+done
 
 #gitignore整備
 ls -l ~/script_env | grep -P '^d' | awk '{print $9}' | xargs -I@ echo cp ~/script_env/.gitignore ~/script_env/@/.gitignore | sh
@@ -17,4 +22,5 @@ BUILD_END=$(date '+%s')
 
 BUILD_ELAPSED=$(expr $BUILD_END - $BUILD_START)
 
+printf "\ndocker build process has done.\n"
 echo $BUILD_ELAPSED seconds.
