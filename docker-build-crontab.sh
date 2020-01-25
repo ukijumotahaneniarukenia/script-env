908411006a91e763e00772043674858cdeeac500
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-01-25 23:03:34 +0900

    環境構築

--- docker-build-crontab.sh
+++ docker-build-crontab.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-git-commit(){
-  #gitignore整備
-  ls -l ~/script_env | grep -P '^d' | awk '{print $9}' | xargs -I@ echo cp ~/script_env/.gitignore ~/script_env/@/.gitignore | sh
-  cd ~/script_env
-  git add .gitignore
-  git add --all *
-  git commit -m "環境構築"
-}
-
 BUILD_START=$(date '+%s')
 
-bash ~/script_env/docker-build-parallel.sh && git-commit
+bash ~/script_env/docker-build-parallel.sh
+
+#gitignore整備
+ls -l ~/script_env | grep -P '^d' | awk '{print $9}' | xargs -I@ echo cp ~/script_env/.gitignore ~/script_env/@/.gitignore | sh
+cd ~/script_env
+git add .gitignore
+git add --all *
+git commit -m "環境構築"
 
 BUILD_END=$(date '+%s')
 
