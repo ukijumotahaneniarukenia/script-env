6bd43fb7b9fb6f5bde007a024e202676f0891707
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-02 09:56:27 +0900

    環境構築

--- docker-build-parallel-retry.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-RETRY_ROUND="$@"
-ls -l ~/script_env | grep -P '^d' | awk '{print $9}' | grep -v docker-build-log | grep -vE $(docker images | tail -n+1 | grep -P '(-[0-9]{1,}){2,}-' | awk '{print $1}'|xargs|tr ' ' '|') | \
-  xargs -P0 -I@ bash -c "echo 'cd ~/script_env/@ && ( time docker build -t @ . ) 1>~/script_env/@/retry-$RETRY_ROUND-log 2>&1 &' " | sh
