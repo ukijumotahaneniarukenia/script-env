05d53a0ebdd2c3d0a2f5d67dee26315f79e5fd71
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-10 20:34:31 +0900

    環境構築

--- 1-rename.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ls -l ~/script-env | grep -P '^d' | awk '{print $9}' | grep -v docker-build-log | xargs -I@ echo mv ~/script-env/@/manual.md ~/script-env/@/man.md
--- 7-select-app-name.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ls ~/script-env | grep -vP '(sh|md|log|crontab)$' | perl -pe 's/[a-z]{1,}(-[0-9]{1,}){1,}-//g;s/-/\n/g;' | sort | uniq
