2acbedf955680561fb773c1f58a08c5e35f51ed6
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-11 11:11:09 +0900

    環境構築

---
+++ 06-script-sketchレポジトリのディレクトリ作成.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ls ~/script-env | grep -vP '(sh|md|log|crontab)$' | grep -vP '^[0-9]{1,}' | perl -pe 's/[a-z]{1,}(-[0-9]{1,}){1,}-//g;s/-/\n/g;' | sort | uniq | xargs -I@ echo mkdir -p ~/script-sketch/@
