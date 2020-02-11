b9cf8466fbf9acb0d30b25b7732b389d067f566a
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-11 12:02:13 +0900

    環境構築

--- 07-script-sketchレポジトリのテンプレートファイル作成.sh
+++ 07-script-sketchレポジトリのテンプレートファイル作成.sh
@@ -3,7 +3,7 @@
 while read tgt;do
   #前回実行時の連番をコメントアウトで残す
   echo mkdir -p $HOME/script-sketch/$tgt
-  seq 1 10 | xargs -I@ printf "touch $HOME/script-sketch/$tgt/%05g-%s-用途名-alias名.拡張子\n" @ $tgt
+  seq 1 10 | xargs -I@ printf "touch $HOME/script-sketch/$tgt/%05g-%s-用途名-non-alias名-alias名.拡張子\n" @ $tgt
   #seq 11 20 | xargs -I@ printf "touch $HOME/script-sketch/$tgt/%05g-%s-用途名-alias名.拡張子\n" @ $tgt
   #seq 21 30 | xargs -I@ printf "touch $HOME/script-sketch/$tgt/%05g-%s-用途名-alias名.拡張子\n" @ $tgt
   #seq 31 40 | xargs -I@ printf "touch $HOME/script-sketch/$tgt/%05g-%s-用途名-alias名.拡張子\n" @ $tgt
--- ubuntu-19-10-java-apache-solr-vim/env.md
+++ ubuntu-19-10-java-apache-solr-vim/env.md
@@ -1,5 +1,5 @@
 ```
-EXPOSE=
-SHM_SIZE=2gb
---build-arg PYTHON_VERSION=3-7-4 --build-arg GIT_VERSION=2-24-1
+EXPOSE=-p 8983:8983
+SHM_SIZE=6gb
+--build-arg PYTHON_VERSION=3-7-4 --build-arg GIT_VERSION=2-24-1 --build-arg JAVA_VERSION=11
 ```
