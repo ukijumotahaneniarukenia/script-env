af026546f26271f5dc02e5678fd925ef3de173e5
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-11 11:20:46 +0900

    環境構築

---
+++ 09-作成済イメージで日付が本日以内であるものを抽出.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker images | grep -P '(?:-[0-9]){1,}' | awk '{print $1}' | xargs -I@ bash -c "echo @ && docker history --human=false @ | sort -rk2 | sed -r 's;\s{1,}; ;g;' | cut -d' ' -f2 | sed -n '2p'" | xargs -n2 | grep "$(date "+%Y-%m-%d")" | cut -d' ' -f1
