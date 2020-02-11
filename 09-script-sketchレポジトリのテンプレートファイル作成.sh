7829b32f3882a93a4a11d4c9118510b5bee27152
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-11 10:18:50 +0900

    環境構築

---
+++ 09-script-sketchレポジトリのテンプレートファイル作成.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+while read tgt;do
+  #前回実行時の連番をコメントアウトで残す
+  echo mkdir -p $HOME/script-sketch/$tgt
+  seq 1 10 | xargs -I@ printf "touch $HOME/script-sketch/$tgt/%05g-%s-用途名-alias名.拡張子\n" @ $tgt
+  #seq 11 20 | xargs -I@ printf "touch $HOME/script-sketch/$tgt/%05g-%s-用途名-alias名.拡張子\n" @ $tgt
+  #seq 21 30 | xargs -I@ printf "touch $HOME/script-sketch/$tgt/%05g-%s-用途名-alias名.拡張子\n" @ $tgt
+  #seq 31 40 | xargs -I@ printf "touch $HOME/script-sketch/$tgt/%05g-%s-用途名-alias名.拡張子\n" @ $tgt
+
+done < <(ls ~/script-env | grep -vP '(sh|md|log|crontab)$' | grep -vP '^[0-9]{1,}' | perl -pe 's/[a-z]{1,}(-[0-9]{1,}){1,}-//g;s/-/\n/g;' | sort | uniq)
--- 09-select-script-env-name.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#ディレクトリ作成。ファイルはそのまま残ってる。
-ls ~/script-env | grep -vP '(sh|md|log|crontab)$' | grep -vP '^[0-9]{1,}' | xargs -I@ echo mkdir -p ~/script-scratch/@
-#新規ファイル作成。
-ls ~/script-env | grep -vP '(sh|md|log|crontab)$' | grep -vP '^[0-9]{1,}' | xargs -I@ echo touch ~/script-scratch/@/README.md
