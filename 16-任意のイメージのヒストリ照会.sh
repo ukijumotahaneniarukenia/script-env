a33ff8f67ac2d3d2ea5511877b0d5b3ef66e5a39
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-12 20:28:26 +0900

    環境構築

--- 16-任意のイメージのヒストリ照会.sh
+++ 16-任意のイメージのヒストリ照会.sh
@@ -4,4 +4,4 @@ TGT="$@"
 
 DEFAULT="(?:-[0-9]){1,}" #全て出力
 
-docker images | grep -P '(?:-[0-9]){1,}' | grep -P "${TGT:-$DEFAULT}" |awk '{print $1}' | xargs -t -I@ docker history --human=false @
+docker images | grep -P '(?:-[0-9]){1,}' | grep -P "${TGT:-$DEFAULT}" |awk '{print $1}' | xargs -I@ bash -c "echo @ && docker history --human=false @"
