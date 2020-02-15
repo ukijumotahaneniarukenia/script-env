a65415045abe5ba9897ebc3492d082d592b841ca
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-15 09:21:40 +0900

    環境構築

--- 21-一括置換対象確認.sh
+++ 21-一括置換対象確認.sh
@@ -3,9 +3,23 @@
 #よさげだったら、以下で一括置換
 #
 #./21-一括置換対象確認.sh | awk '{$1="";$2="";print $0}' | sh
+usage(){
+cat <<EOS
+Usage:
+  ./21-一括置換対象確認.sh '~' '\\\$HOME'
+
+EOS
+}
+
+
+IS_AS=$1
+TO_BE=$2
+
+[ -z "$IS_AS" ] && usage && exit 1
+[ -z "$TO_BE" ] && usage && exit 1
 
 while read tgt;do
 
-  printf "%s\t%s\t%s\n" $(grep -c -P '~' $tgt) $tgt "sed -i 's;~;\$HOME;g' $tgt" | grep -vP "^0|${0#./}"
+  printf "%s\t%s\t%s\n" $(grep -c -P "$IS_AS" $tgt) $tgt "sed -i 's;$IS_AS;$TO_BE;g' $tgt" | grep -vP "^0|${0#./}"
 
 done < <(find $HOME/script-env -type f -name "*sh" -o -name "*Dockerfile")
