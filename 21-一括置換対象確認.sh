6a82c042db47252bb0d1039880cb0314f9be13ec
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-15 09:04:03 +0900

    環境構築

--- 21-一括置換対象確認.sh
+++ 21-一括置換対象確認.sh
@@ -2,6 +2,6 @@
 
 while read tgt;do
 
-  printf "%s\t%s\n" $(grep -c -P '~' $tgt) $tgt | grep -v '^0'
+  printf "%s\t%s\t%s\n" $(grep -c -P '~' $tgt) $tgt "sed -i 's;~;\$HOME;g' $tgt" | grep -vP '^0'
 
 done < <(find $HOME/script-env -type f -name "*sh" -o -name "*Dockerfile")
