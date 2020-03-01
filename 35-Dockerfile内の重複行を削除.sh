4cb12bb43ce8b4a0e02f81f46a7a6a71c3e087c5
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-01 11:21:41 +0900

    環境構築

---
+++ 35-Dockerfile内の重複行を削除.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while read tgt;do
+  echo "echo $HOME/script-env/$tgt/Dockerfile"
+  echo "cd  $HOME/script-env/$tgt && uniq Dockerfile>Dockerfile.$$"
+  echo "cd  $HOME/script-env/$tgt && diff Dockerfile*"
+  echo "cd  $HOME/script-env/$tgt && mv Dockerfile.$$ Dockerfile"
+done < <(ls -l $HOME/script-env | grep -P '^d' | awk '{print $9}' | grep -v docker-build-log) | bash
