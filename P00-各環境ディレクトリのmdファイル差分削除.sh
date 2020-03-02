e3088cd6444ae1955fd54714e0de93f11c409678
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-03 07:19:56 +0900

    環境構築

--- P00-各環境ディレクトリのmdファイル差分削除.sh
+++ P00-各環境ディレクトリのmdファイル差分削除.sh
@@ -7,4 +7,4 @@ while read tgt;do
   #find $HOME/script-env -name "*md" | grep -vP '(-[0-9]){1,}' | perl -pe 's;.*/;;g' | grep -vP "$(find $HOME/script-env/$tgt -name "*md" | perl -pe 's;.*/;;g'|xargs|tr ' ' '|')" | \
   #  xargs -I@ echo touch $HOME/script-env/$tgt/@
 
-done < <(ls -l $HOME/script-env | grep -P '^d' | awk '{print $9}' | grep -v docker-build-log) 
+done < <(ls -l $HOME/script-env | grep -P '^d' | awk '{print $9}' | grep -v docker-build-log)
