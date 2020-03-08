d7d296b92e519806afa40db30d9506a94f7a70d4
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-08 16:28:41 +0900

    環境構築

--- 41-Dockerfile.subのクレンジング.sh
+++ 41-Dockerfile.subのクレンジング.sh
@@ -3,20 +3,25 @@
 usage(){
 cat <<EOS
 Usage:
-  $0 Dockerfile.sub-stop-word-list
+  $0 script-env Dockerfile.sub-stop-word-list
 EOS
 exit 0
 }
 
-STOP_WORD_LIST=$1
+REPO=$1;shift
+STOP_WORD_LIST=$1;shift
 
+[ -z $REPO ] && usage
 [ -z $STOP_WORD_LIST ] && usage
 
 while read tgt;do
-  echo $tgt
-  #cat $tgt
-  while read stop_word;do
-    #echo $stop_word
-    echo "sed -i /$stop_word/d $tgt" | bash
-  done < $STOP_WORD_LIST
-done < <(find $HOME/script-env -name "Dockerfile.sub" | grep -vP oracle)
+  if [ -f $tgt/Dockerfile.sub.done ];then
+    #Dockerfile.sub.doneが存在する場合はクレンジングをしない
+    :
+  else
+    #Dockerfile.sub.doneが存在しない場合はクレンジングをする
+    while read stop_word;do
+      echo "sed -i /$stop_word/d $tgt/Dockerfile.sub" | bash
+    done < $STOP_WORD_LIST
+  fi
+done < <(find $HOME/$REPO -mindepth 1 -type d | grep -vP '\.git|docker-log')
