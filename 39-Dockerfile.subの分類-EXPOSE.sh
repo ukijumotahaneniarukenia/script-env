4816575ac832124e51b3604afe06044db0dc7373
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-08 21:30:46 +0900

    環境構築

--- 39-Dockerfile.subの分類-EXPOSE.sh
+++ 39-Dockerfile.subの分類-EXPOSE.sh
@@ -13,6 +13,21 @@ CHK_WORD=$1
 [ -z $CHK_WORD ] && usage
 
 while read tgt;do
-  echo $tgt
-  grep $CHK_WORD $tgt | tr ' ' '='
-done < <(find . -name "Dockerfile.sub" | grep -vP oracle)
+  if [ -f $tgt/Dockerfile.sub ];then
+    RT="$(grep $CHK_WORD $tgt/Dockerfile.sub | tr ' ' '=')"
+    if [ -z "$RT" ];then
+      :
+    else
+      while read n;do
+        echo "echo '-p $n' >>$tgt/env-expose.md" | bash
+      done < <(echo $RT | perl -pe 's/([0-9]+)/\1:\1/g;s/EXPOSE=//g'| tr ' ' '\n')
+    fi
+  else
+    :
+  fi
+  if [ -f $tgt/Dockerfile.sub ];then
+    echo "sed -i /^$/d $tgt/env-expose.md" | bash
+  else
+    :
+  fi
+done < <(find $HOME/script-env -mindepth 1 -type d | grep -vP '\.git')
