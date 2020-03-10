006025f1c6c00a0ad39c4055049ce81178a3c5d7
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-11 00:43:02 +0900

    環境構築

--- 53-レポに存在するファイルが環境ディレクトリ内のDockerfile内に存在するかチェックしなければリネームする.sh
+++ 53-レポに存在するファイルが環境ディレクトリ内のDockerfile内に存在するかチェックしなければリネームする.sh
@@ -21,16 +21,9 @@ while read dir;do
   OS_VERSION=$(echo $dir | grep -Po '[a-z]+(-[0-9]{1,}){1,}')
   while read arg;do
     while read cmd;do
-      n=$HOME/$INSTALLER_REPO/$(echo "( export $arg;export OS_VERSION=$OS_VERSION;export REPO=$INSTALLER_REPO;echo \"$cmd\")" | bash | grep -Po "$OS_VERSION.+\.sh")
-      while read repo;do
-        if [ $repo == $n ];then
-          echo 1 $n $repo
-        else
-          echo 0 $n $repo
-        fi
-      done < <(find $HOME/$INSTALLER_REPO -type f | grep -vP '\.git|md$')
+      echo "( export $arg;export OS_VERSION=$OS_VERSION;export REPO=$INSTALLER_REPO;echo \"$cmd\")" | bash | grep -Po "$OS_VERSION.+\.sh"
     done < <(grep VERSION.sh $dir/Dockerfile.done)
-  done < <(grep VERSION $dir/env-build-arg.md) | grep -P '^1'
+  done < <(grep VERSION $dir/env-build-arg.md) | sort | uniq
 done < <(find $HOME/$ENV_REPO -mindepth 1 -type d | grep -vP '\.git|docker-log' )
 
 #Dockerfile.doneにあってenv-build-arg.mdにないもの
