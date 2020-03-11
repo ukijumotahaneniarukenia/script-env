fb6a74ef242a3012169593d573ade2fb540e5fb3
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-12 00:30:10 +0900

    環境構築

--- 555-common-copy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#環境ディレクトリに移動して
-cd $HOME/script-env/ubuntu-16-04-postgres-python-vim
-
-#OSバージョン別にファイル作成（ことなる可能性があるので。デフぉはほぼおなじなはずとしんじている）
-grep -Po '(?<=OS_VERSION-)(.*)(?=\.sh)' Dockerfile | xargs -I@ echo "cp $HOME/script-repo/ubuntu-19-10-@.sh $HOME/script-repo/ubuntu-16-04-@.sh"
