2f308f8259a01778ec5773105643e903fed033c7
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-11 20:57:15 +0900

    環境構築

--- 38-Dockerfile.subの分類-ARG.sh
+++ 38-Dockerfile.subの分類-ARG.sh
@@ -3,28 +3,37 @@
 usage(){
 cat <<EOS
 Usage:
-  $0 ARG
+  $0 ARG script-env script-repo
 EOS
 exit 0
 }
 
-CHK_WORD=$1
+CHK_WORD=$1;shift
+ENV_REPO=$1;shift
+INSTALLER_REPO=$1;shift
 
 [ -z $CHK_WORD ] && usage
+[ -z $ENV_REPO ] && usage
+[ -z $INSTALLER_REPO ] && usage
 
 while read tgt;do
-  echo $tgt
-  KEY=$(grep $CHK_WORD $tgt | awk '{print $2}')
-  WORD=$(grep $CHK_WORD $tgt | awk '{print $2}' | perl -pe 's/VERSION//' | perl -pe 's/_/-/g')
-  while read f;do
-    [ -z ${WORD} ] && continue
-    if [[ ${f} =~ ${WORD,,}.* ]]; then
-      {
-        echo ${KEY}
-        echo ${BASH_REMATCH[0]} | perl -pe 's/\.sh//g' | grep -Po '(-[0-9]+){1,}' | perl -pe 's/^-//'
-      } | xargs -n2 | tr ' ' '='
-    else
-      :
-    fi
-  done < <(find $HOME/script-repo -type f | grep -vP '\.git')
-done < <(find . -name "Dockerfile.sub" | grep -vP oracle)
+  if [ -f $tgt/Dockerfile.sub ];then
+    #Dockerfile.subが存在する場合、INSTALLER_REPOに存在するバージョンのでkey:valueペアを作成し、各環境ディレクトリのenv-build-arg.mdに追記
+    KEY=$(grep $CHK_WORD $tgt/Dockerfile.sub | awk '{print $2}')
+    WORD=$(grep $CHK_WORD $tgt/Dockerfile.sub | awk '{print $2}' | perl -pe 's/VERSION//' | perl -pe 's/_/-/g')
+    while read f;do
+      [ -z ${WORD} ] && continue
+      if [[ ${f} =~ ${WORD,,}.* ]]; then
+        {
+          echo ${KEY}
+          echo ${BASH_REMATCH[0]} | perl -pe 's/\.sh//g' | grep -Po '(-[0-9]+){1,}' | perl -pe 's/^-//'
+        } | xargs -n2 | tr ' ' '='
+      else
+        :
+      fi
+    done < <(find $HOME/$INSTALLER_REPO -type f | grep -vP '\.git')
+  else
+    #何もしないsubファイルを作成してほしいい
+    :
+  fi
+done < <(find $HOME/$ENV_REPO -mindepth 1 -type d | grep -vP '\.git|docker-log')
--- P09-Dockerfile.subの作成.sh
+++ P09-Dockerfile.subの作成.sh
@@ -8,32 +8,20 @@ EOS
 exit 0
 }
 
-execute(){
+ENV_REPO="$1";shift
 
-  OS_VERSION=$1;shift
+[ -z $ENV_REPO ] && usage
 
-  [ -z $OS_VERSION ] && usage
-
-  #テンプレート生成したDockerfileと既存Dockerfileのうち既存にしかないものを抽出
-  while read tgt;do
-    if [ -f $tgt/Dockerfile.sub.done ];then
-      #Dockerfile.sub.doneが存在する場合はDockerfile.subを作成しない
-      : #echo 1 $tgt
-    elif [ -f $tgt/Dockerfile.sub.undone ];then
-      #Dockerfile.sub.undoneが存在する場合は着手したが完了していない状態なので、Dockerfile.subを作成しない。
-      : #echo 1 $tgt
-    else
-      #echo 0 $tgt
-      echo "cd  $tgt && comm -23 --nocheck-order <(sort Dockerfile | sed -r '/^$/d') <(sort Dockerfile.auto | sed -r '/^$/d')>Dockerfile.sub"
-    fi
-  done < <(find $HOME/$REPO -type d | grep -v docker-log | grep $OS_VERSION) | bash
-}
-
-main(){
-  REPO="$@"
-  [ -z $REPO ] && usage
-  export -f execute
-  find $HOME/$REPO -type d | grep -Po '[a-z]+(-[0-9]{1,}){1,}' | sort | uniq | while read tgt;do execute $tgt;done
-}
-
-main "$@"
+#自動生成したDockerfileと既存Dockerfileのうち既存にしかないものを抽出
+while read tgt;do
+  if [ -f $tgt/Dockerfile.sub.done ];then
+    #Dockerfile.sub.doneが存在する場合はDockerfile.subを作成しない
+    : #echo 1 $tgt
+  elif [ -f $tgt/Dockerfile.sub.undone ];then
+    #Dockerfile.sub.undoneが存在する場合は着手したが完了していない状態なので、Dockerfile.subを作成しない。
+    : #echo 1 $tgt
+  else
+    #echo 0 $tgt
+    echo "cd  $tgt && comm -23 --nocheck-order <(sort Dockerfile | sed -r '/^$/d') <(sort Dockerfile.auto | sed -r '/^$/d')>Dockerfile.sub"
+  fi
+done < <(find $HOME/$ENV_REPO -mindepth 1 -type d | grep -vP '\.git|docker-log')
