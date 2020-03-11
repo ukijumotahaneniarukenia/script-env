0dd4840b8b864a373d70b7c520bb66ee72ec5a09
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-11 23:58:57 +0900

    環境構築

--- P01-各環境ディレクトリのmdファイル差分更新.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while read tgt;do
-
-  #echo $tgt
-  find $HOME/script-env -name "*md" | grep -vP '(-[0-9]){1,}' | perl -pe 's;.*/;;g' | grep -vP 'systemd' | grep -vP "$(find $HOME/script-env/$tgt -name "*md" | perl -pe 's;.*/;;g'|xargs|tr ' ' '|')" | \
-    xargs -I@ echo touch $HOME/script-env/$tgt/@
-
-done < <(ls -l $HOME/script-env | grep -P '^d' | awk '{print $9}' | grep -v docker-log)
