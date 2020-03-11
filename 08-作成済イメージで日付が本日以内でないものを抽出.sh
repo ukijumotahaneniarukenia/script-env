f38fa1fcb49240fa2a15040f262c876010021f9b
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-12 07:41:37 +0900

    環境構築

--- 08-作成済イメージで日付が本日以内でないものを抽出.sh
+++ 08-作成済イメージで日付が本日以内でないものを抽出.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker images | grep -P '(?:-[0-9]){1,}' | awk '{print $1}' | xargs -I@ bash -c "echo @ && docker history --human=false @ | sort -rk2 | sed -r 's;\s{1,}; ;g;' | cut -d' ' -f2 | sed -n '2p'" | xargs -n2 | grep -v "$(date "+%Y-%m-%d")" | cut -d' ' -f1
+docker images | awk '{print $1}' | grep -P '(?:-[0-9]+){1,}' | xargs -I@ bash -c "echo @ && docker history --human=false @ | sort -rk2 | sed -r 's;\s{1,}; ;g;' | cut -d' ' -f2 | sed -n '2p'" \
+  | xargs -n2 \
+  | grep -v "$(date "+%Y-%m-%d")" \
+  #| cut -d' ' -f1
+
+#docker images | awk '{print $1}'| grep -P '(?:-[0-9]){1,}'  | xargs -I@ bash -c "echo @ && docker history --human=false @ | sort -rk2 | sed -r 's;\s{1,}; ;g;' | cut -d' ' -f2 | sed -n '2p'" | xargs -n2 | grep -v "$(date "+%Y-%m-%d")" | cut -d' ' -f1
