51d52bbe19be0c2ab84e403da86d85d52e13e5c8
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-17 07:38:01 +0900

    環境構築

--- 09-作成済イメージで日付が本日以内であるものを抽出.sh
+++ 09-作成済イメージで日付が本日以内であるものを抽出.sh
@@ -1,3 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-docker images | grep -P '(?:-[0-9]){1,}' | awk '{print $1}' | xargs -I@ bash -c "echo @ && docker history --human=false @ | sort -rk2 | sed -r 's;\s{1,}; ;g;' | cut -d' ' -f2 | sed -n '2p'" | xargs -n2 | grep "$(date "+%Y-%m-%d")" | cut -d' ' -f1
+docker images | awk '{print $1}' | grep -P '(?:-[0-9]+){1,}' | while read tgt;do \
+  echo $tgt
+  MKDTM=$(docker history --human=false $tgt | sort -rk2 | sed -r 's;\s{1,}; ;g;' | cut -d' ' -f2 | sed -n '2p')
+  echo $MKDTM
+done | xargs -n2 | grep "$(date "+%Y-%m-%d")" | column -t -s' '
