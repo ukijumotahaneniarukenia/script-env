a54d6f4cf784583309284e8274a62280b1187270
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-05-08 00:21:27 +0900

    sketch

--- ubuntu-19-10-dbus-vim/run.sh
+++ ubuntu-19-10-dbus-vim/run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+#このスクリプトは１回のみ実行したいスクリプトを記載
 
-dbus-daemon --config-file=/etc/dbus-1/myCustomDbus.conf --print-address &
+#インプットメソッドサーバの自動起動設定rootユーザーで実行している
+fcitx 1>$HOME/launch-fcitx.log 2>&1
 
+#ログイン時のシェル設定
 /usr/bin/env bash
