c7aa06d4e1c0f6895d42dd26f6075d01b48d0a7d
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-05-13 22:03:35 +0900

    sketch

--- ubuntu-19-10-wine-vim/run.sh
+++ ubuntu-19-10-wine-vim/run.sh
@@ -2,7 +2,10 @@
 #このスクリプトは１回のみ実行したいスクリプトを記載
 
 #インプットメソッドサーバの自動起動設定rootユーザーで実行している
-fcitx 1>$HOME/launch-fcitx.log 2>&1
+#fcitx 1>$HOME/launch-fcitx.log 2>&1
+
+ls /home/aine/media
+
 
 #ログイン時のシェル設定
 /usr/bin/env bash
