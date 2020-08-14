9050334a2df749a72dcf6979c572a02c20f32bcd
ukijumotahaneniarukenia <48931754+ukijumotahaneniarukenia@users.noreply.github.com>
2020-08-14 22:56:50 +0900

    Update my-installer-ubuntu.sh

--- my-installer-ubuntu.sh
+++ my-installer-ubuntu.sh
@@ -1,64 +1,48 @@
-デスクトップの日本語化
+#デスクトップの日本語化
 
-ここは対話入力
+#対話入力
 
-```
 LANG=C xdg-user-dirs-gtk-update
-```
 
-rootユーザーのパスワード設定
+#rootユーザーのパスワード設定
 
-ここも対話入力
+#対話入力
 
-```
 sudo passwd root
 
 su root
 
 id
 #uid=0(root) gid=0(root) groups=0(root)
-```
 
 
+#chromeのインストール
 
+#rootユーザーで実行
 
-chromeのインストール
-
-rootユーザーで実行
-
-- https://qiita.com/spiderx_jp/items/e6189a736ddec14ffa23
-
-```
-
+#https://qiita.com/spiderx_jp/items/e6189a736ddec14ffa23
 
 sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 apt update
-apt-get install -y google-chrome-stable
+apt install -y google-chrome-stable
 
-```
+#dokcerのインストール
 
-dokcerのインストール
-
-rootユーザーで実行
+#rootユーザーで実行
 
 - https://qiita.com/tkyonezu/items/0f6da57eb2d823d2611d
 
-
-```
 #古いバージョンの削除
-apt-get remove docker docker-engine docker.io containerd runc
-
-
+apt remove docker docker-engine docker.io containerd runc
 
 #必要なパッケージのインストール
-apt-get -y install \
+apt -y install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common
-
 
 #GPG 公開鍵のインストール
 
@@ -82,10 +66,10 @@ add-apt-repository \
 
 
 #docker-ceのインストール
-apt-get update
+apt update
 
 
-apt-get install -y docker-ce
+apt install -y docker-ce
 
 
 #一般ユーザーをdockerグループに割り付ける
@@ -108,28 +92,15 @@ systemctl | grep docker
 
 #マシン再起動（一般ユーザでも実行できるか確認）
 
-
-自作コマンドの登録など
-
-```
-mkdir -p $HOME/.local/bin #ベンダー系
-mkdir -p $HOME/.local/script-cmd/bin #ベンダー系以外
-mkdir -p $HOME/.local/script-search/bin #ベンダー系以外
-find $HOME/script-cmd -type f -name "mysearch-register-bash" -o -name "mycmd-register-bash" | bash | bash
-```
-
-
-
-
 #動作確認
 
 docker run hello-world
-```
+
 
 #vim本体のインストール
 
 #rootユーザーで実行
-```
+
 cd /usr/local/src
 
 git clone https://github.com/ukijumotahaneniarukenia/script-repo.git
@@ -137,17 +108,19 @@ git clone https://github.com/ukijumotahaneniarukenia/script-repo.git
 
 cd /usr/local/src/script-repo
 
-
 bash ubuntu-19-10-install-vim-system.sh
-```
 
 #ユーザー環境のインストール
 
-```
 #rootユーザー,一般ユーザー用
 bash ubuntu-19-10-install-vim-user.sh
 bash ubuntu-19-10-install-vim_plug.sh
 bash ubuntu-19-10-config-dotfile.sh
 bash ubuntu-19-10-config-env.sh
 
-```
+#自作コマンドの登録など
+
+mkdir -p $HOME/.local/bin #ベンダー系
+mkdir -p $HOME/.local/script-cmd/bin #ベンダー系以外
+mkdir -p $HOME/.local/script-search/bin #ベンダー系以外
+find $HOME/script-cmd -type f -name "mysearch-install-bash" -o -name "mycmd-install-bash" | bash
